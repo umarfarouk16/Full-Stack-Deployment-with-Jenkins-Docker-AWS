6b826d18d1fcd21c579a89fc39f0fbaaa90ea83d
Umar Farouk <umarmuftau16@gmail.com>
2026-03-01 04:30:22 -0500

    fix: export CORS_ORIGIN as string to match index.js import

--- techpathway-2/backend/config.js
+++ techpathway-2/backend/config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    corsOrigins: [
-      'http://localhost:3000',
-      'http://techpathway-backend-alb-454838952.us-east-1.elb.amazonaws.com', // From terraform output
-      '*'
-    ],
+    CORS_ORIGIN: '*',
     port: process.env.PORT || 8080
-  };
+};
